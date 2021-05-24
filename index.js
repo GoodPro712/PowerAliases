3deb94fd7aa4cb4c7f0b5ae441c7910ee763307f
LandenStephenss <turtlederp806@icloud.com>
2021-05-23 20:00:10 -0500

    upload before pull because yes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Plugin } = require("powercord/entities");
+const { getModule } = require("powercord/webpack");
+const { inject, uninject } = require('powercord/injector');
 const Settings = require("./components/settings");
 module.exports = class PowerAliases extends Plugin {
   async startPlugin() {
@@ -8,8 +10,25 @@ module.exports = class PowerAliases extends Plugin {
       label: "PowerAliases",
       render: Settings,
     });
+
+    var messageEvents = getModule(["sendMessage"]);
+
+    inject(
+      "PowerAliases-Chat",
+      messageEvents,
+      "sendMessage",
+      function (args) {
+        const text = args[1].content;
+
+        console.log(text);
+        args[1].content = text;
+        return args;
+      },
+      true
+    );
   }
   pluginWillUnload() {
     powercord.api.settings.unregisterSettings(this.entityID);
+    uninject("PowerAliases-Chat");
   }
 };
